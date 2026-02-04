81bbc6747b044ed9a7e46317a1b906e12260f3a0
Tomasz Unger <tomasz.unger@yahoo.pl>
2026-02-04 14:06:03 +0100

    Pierwszy commit

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int myFavoriteNumber = 0;
+
+    printf("What is your favorite number : ");
+    scanf("%d", &myFavoriteNumber);
+    printf("Your favorite number is %d\n", myFavoriteNumber);
+
+    return 0;
+}
